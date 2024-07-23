7c30f6fa41acefd434b508a0905fcc3b367b1be1
Said Gabriel(Pinglinux) <163392959+gabri203@users.noreply.github.com>
2024-07-23 16:14:28 +0200

    Add files via upload

---
+++ Rilevare_numero_positivo_negativo.py
@@ -0,0 +1,8 @@
+numero=float(input("Inserisce un numero : "))
+
+if numero > 0:
+        print('Numero positivo')
+elif numero < 0:
+        print('Numero negativo')
+elif numero == 0:
+        print('Numero neutrale')
